5c93810c083abf407cc8ef6066ecf44dff5f0d61
jean-mulet <leo0609leo@gmail.com>
2024-11-20 12:58:22 +0100

    feat(): init

---
+++ src/main/java/leo_carre/garcia/N00b_Save/N00bSaveApplication.java
@@ -0,0 +1,13 @@
+package leo_carre.garcia.N00b_Save;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N00bSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N00bSaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leo_carre/garcia/N00b_Save/N00bSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package leo_carre.garcia.N00b_Save;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N00bSaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
